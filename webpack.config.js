d9833e462e41b5055bdc7dc7f9a0977e8f89dc84
Elbo <marco.botto@gmail.com>
2016-08-24 00:41:21 +0200

    add(Webpack): Set up build script

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path.join(__dirname, 'src', 'index.js')
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'docs'),
     pathinfo: true,
     filename: 'bundle.js',
     publicPath: '/'
